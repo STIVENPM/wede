285179933542e4afa0528a84a9ab2a6455f1b5cb
Jose Stiven Perdomo <mrtinez.stivengmail.com>
2026-04-13 11:24:03 -0500

    inserts

---
+++ 09_inserts/02-inserts-person.sql
@@ -0,0 +1,2 @@
+INSERT INTO person (id, first_name, last_name, birth_date, email, created_by)
+VALUES (1, 'juan', 'perez', '1990-05-15', 'juan.perez@example.com', '550e8400-e29b-41d4-a716-446655440000');
